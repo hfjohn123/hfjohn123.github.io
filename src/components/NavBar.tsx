45570ed9c9aae829b1c7732a4051be7e70110597
hfjoh <hfjohn123@live.com>
2024-03-23 22:23:08 -0400

    Tweek CSS import a little bit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -21,7 +21,7 @@ export default function NavBar({ className }: { className?: string }) {
     middleware: [flip(), shift()],
   });
   return (
-    <div className={`bg-gray-800 sticky z-50 top-0 ${className} print:hidden`}>
+    <div className={`bg-gray-800 sticky z-50 top-0 ${className}`}>
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
